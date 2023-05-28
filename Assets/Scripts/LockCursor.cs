8d5c31c4c486353bdf4ce1630d0379fe1b4623c6
Alexandre Molina <alexandre.molina@epitech.eu>
2023-05-28 11:12:31 +0200

    [ADD] Pause Menu , Resume & Quit game

---
+++ Assets/Scripts/LockCursor.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:672ae3d34c8fc4fe34efec0780dceca40b6d1ec11b8bfa966d5a6172199e7929
+size 314
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fa140caecc534e78a7d870db2073cd09f375b6d32f1e8d3c69e575edc75b932b
+size 1243
